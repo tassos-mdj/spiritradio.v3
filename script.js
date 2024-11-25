91cad8b1586c03ee6a62549fde9aecbaa539d233
tassos-mdj <microdj@proton.me>
2024-11-25 12:52:02 +0200

    Working on show alert modal

--- script.js
+++ script.js
@@ -2,32 +2,32 @@ const playCheckbox = document.getElementById("checkbox");
 const loader = document.querySelector(".loader");
 const audio = document.querySelector("audio");
 loader.style.display = "none";
-document.getElementById("checkbox_label").addEventListener("click", function() {
+document.getElementById("checkbox_label").addEventListener("click", function () {
     if (playCheckbox.checked == false) {
         loader.style.display = "flex";
         audio.play();
-        
+
     } else {
         loader.style.display = "none";
         audio.pause();
         audio.load();
-        
+
     }
 });
 
 
 // Reload page when visited with browser's "Back" button
-window.addEventListener( "pageshow", function ( event ) {
-    var historyTraversal = event.persisted || 
-                           ( typeof window.performance != "undefined" && 
-                                window.performance.navigation.type === 2 );
-    if ( historyTraversal ) {
-      // Handle page restore.
-      window.location.reload();
+window.addEventListener("pageshow", function (event) {
+    var historyTraversal = event.persisted ||
+        (typeof window.performance != "undefined" &&
+            window.performance.navigation.type === 2);
+    if (historyTraversal) {
+        // Handle page restore.
+        window.location.reload();
     }
-  });
+});
 
- // Specify the API endpoint for user data
+// Specify the API endpoint for user data
 apiUrl = 'https://a8.asurahosting.com/api/nowplaying/spirit_radio';
 
 const nowPlaying = document.querySelector(".now-playing");
@@ -36,23 +36,47 @@ const nowPlaying = document.querySelector(".now-playing");
 function get_now_playing() {
 
     fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-        throw new Error('Network response was not ok');
-        }
-        return response.json();
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
         })
-    .then(userData => {
-        // Process the retrieved user data
-        nowPlaying.textContent = userData.now_playing.song.text;
-        
+        .then(userData => {
+            // Process the retrieved user data
+            nowPlaying.textContent = userData.now_playing.song.text;
+
         })
-    .catch(error => {
-        console.error('Error:', error);
+        .catch(error => {
+            console.error('Error:', error);
         });
 
-  setTimeout(() => get_now_playing(), 30000)
+    setTimeout(() => get_now_playing(), 30000)
 }
 
 get_now_playing();
- 
+
+
+//modal functionality
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+// const closeModalBtn = document.querySelector(".btn-close");
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+document.onload(openModal());
+closeModalBtn.addEventListener("click", closeModal());
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
